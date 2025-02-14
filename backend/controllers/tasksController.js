ba4ec54573503949037834e97e50123ba27888ab
yogeshwera96 <yogeshwera1996@gmail.com>
2025-02-14 12:49:16 +0530

    add mongodb database and env file

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -1,17 +1,22 @@
+const tasks = [
+  {
+    title: "Task 1",
+    description: "Description of Task 1",
+    status: "Completed",
+  },
+  {
+    title: "Task 2",
+    description: "Description of Task 2",
+    status: "Pending",
+  },
+]
 
-
-exports.getTasks = () => {
-
+exports.getTasks = (req, res) => {
+  res.json(tasks)
 }
 
-exports.addTask = () => {
-    
-}
+exports.addTask = () => {}
 
-exports.deleteTask = () => {
-    
-}
+exports.deleteTask = () => {}
 
-exports.editTask = () => {
-    
-}
+exports.editTask = () => {}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
+require("dotenv").config()
+
 const express = require("express")
 const app = express()
 const tasksRoutes = require("./routes/tasksRoutes")
+const mongoose = require("mongoose")
 
 app.use("/tasks", tasksRoutes)
 
@@ -8,4 +11,15 @@ app.get("/", (req, res) => {
   res.send("<h1> Testing server</h1>")
 })
 
-app.listen(3001, () => console.log("app listening on port 3001"))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("DB is connected successfully")
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log("server listening on port", process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
