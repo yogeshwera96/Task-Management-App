769a0b4aedb536b0f90daad836d82aec46743cef
yogeshwera96 <yogeshwera1996@gmail.com>
2025-02-15 22:38:28 +0530

    css proprtys are updated and Task.js modified

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,5 +1,5 @@
-import axios from "axios"
-import "../App.css"
+import axios from "axios";
+import "../App.css";
 
 const Task = ({
   title,
@@ -13,16 +13,16 @@ const Task = ({
 }) => {
   async function handleDelete() {
     try {
-      const response = await axios.delete(`http://localhost:3001/tasks/${_id}`)
-      console.log("Task deleted", response.data)
-      setTasks(tasks.filter((task) => task._id !== _id))
-      setNotification({ message: "Task deleted successfully!", type: "delete" })
-      setTimeout(() => setNotification(""), 3000)
+      const response = await axios.delete(`http://localhost:3001/tasks/${_id}`);
+      console.log("Task deleted", response.data);
+      setTasks(tasks.filter((task) => task._id !== _id));
+      setNotification({ message: "Task deleted successfully!", type: "delete" });
+      setTimeout(() => setNotification(""), 3000);
     } catch (error) {
       console.error(
         "Error deleting task: ",
         error.response?.data || error.message
-      )
+      );
     }
   }
 
@@ -30,33 +30,37 @@ const Task = ({
     try {
       await axios.patch(`http://localhost:3001/tasks/${_id}`, {
         status: "completed",
-      })
+      });
       setTasks(
         tasks.map((task) =>
           task._id === _id ? { ...task, status: "completed" } : task
         )
-      )
+      );
     } catch (error) {
       console.error(
-        "Error deleting task: ",
+        "Error marking task as complete: ",
         error.response?.data || error.message
-      )
+      );
     }
   }
 
   return (
     <div className="task">
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <h3 className={status === "pending" ? "pending" : "completed"}>
-        {status}
-      </h3>
-      {status === "pending" && (
-        <button onClick={markAsComplete}>Mark as Complete</button>
-      )}
-      <button onClick={handleDelete}>Delete</button>
+      <div className="task-content">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <h3 className={status === "pending" ? "pending" : "completed"}>
+          {status}
+        </h3>
+      </div>
+      <div className="task-actions">
+        {status === "pending" && (
+          <button onClick={markAsComplete}>Mark as Complete</button>
+        )}
+        <button onClick={handleDelete}>Delete</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
